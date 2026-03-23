46b7043e8a27d2b37a3aeecf57d23115380a14c9
schlumphi <j.zarzycki777@gmail.com>
2026-03-23 20:37:07 +0100

    feat: fruit use case

--- module2/task5/main.cpp
+++ module2/task5/main.cpp
@@ -5,5 +5,10 @@
 #include "item.hpp"
 
 int main() {
+    auto apple = Fruit{"apple", 3, 5, 3};
+    std::cout << apple.getName() << ": basePrice: " << apple.getBasePrice() << " amount: " << apple.getAmount() << " price: " << apple.getPrice() << " \n";
+    --apple;
+    std::cout << apple.getName() << ": basePrice: " << apple.getBasePrice() << " amount: " << apple.getAmount() << " price: " << apple.getPrice() << " \n";
+
     return 0;
 }
