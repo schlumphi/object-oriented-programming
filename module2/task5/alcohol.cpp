89a3d12868a95b649b61a211c273cfbccc04ea89
schlumphi <j.zarzycki777@gmail.com>
2026-03-23 20:14:15 +0100

    feat: alcohol class impl

--- module2/task5/alcohol.cpp
+++ module2/task5/alcohol.cpp
@@ -1,16 +1,19 @@
 #include "alcohol.hpp"
 
-Alcohol::Alcohol(const std::string& name, const size_t amount, const size_t basePrice, const double ethanolContent) {
+#include <cmath>
+#include <iostream>
+
+Alcohol::Alcohol(const std::string& name, const size_t amount, const size_t basePrice, const double ethanolContent)
+    : Cargo(name, amount, basePrice), ethanolContent_(validateEthanolContent(ethanolContent)) {
+}
+
+double Alcohol::validateEthanolContent(const double ethanolContent) {
+    if (ethanolContent <= 0.0 || ethanolContent > 96.0) {
+        throw std::invalid_argument("expected 'ethanolContent' to be in range (0, 96.0>");
+    }
+    return ethanolContent;
 }
 
 size_t Alcohol::getPrice() const {
-}
-
-std::string Alcohol::getName() const {
-}
-
-size_t Alcohol::getAmount() const {
-}
-
-size_t Alcohol::getBasePrice() const {
+    return static_cast<size_t>(std::round(static_cast<double>(basePrice_) * ethanolContent_));
 }
--- module2/task5/alcohol.hpp
+++ module2/task5/alcohol.hpp
@@ -6,10 +6,12 @@ public:
     Alcohol(const std::string& name, const size_t amount, const size_t basePrice, const double ethanolContent);
 
     size_t getPrice() const override;
-    std::string getName() const override;
-    size_t getAmount() const override;
-    size_t getBasePrice() const override;
+    std::string getName() const override { return name_; }
+    size_t getAmount() const override { return amount_; }
+    size_t getBasePrice() const override { return basePrice_; }
+
+    static double validateEthanolContent(const double ethanolContent);
 
 private:
-    double ethanolContent;
+    double ethanolContent_;
 };
